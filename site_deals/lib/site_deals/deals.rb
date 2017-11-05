9fb08f714f3be5c4eb77b3d457a9b9f289dace33
Lara Sanders <larasanders@gmail.com>
2017-11-05 23:25:26 +0000

    added deals.rb and created new instances of deal_1 and deal_2

---
+++ site_deals/lib/site_deals/deals.rb
@@ -0,0 +1,22 @@
+class SiteDeals::Deals
+  attr_accessor :name, :price, :availability, :url
+
+  def self.today
+  #   puts "Today's Site Deals"
+  #   puts <<-DOC.gsub /^\s*/, ''
+  #   1. Joie and soft Joie -ruelala
+  #   2. Theory - Gilt
+  # DOC
+  deal_1 = self.new
+  deal_1.name = "Joie and soft Joie"
+  deal_1.availability = true
+  deal_1.url = "https://www.ruelala.com/boutique/collection/105984/192960/?dsi=BTQ-f142a7af-3767-4f15-b365-f6757b52ba5d"
+
+  deal_2 = self.new
+  deal_2.name = "Theory"
+  deal_2.availability = true
+  deal_2.url = "https://www.gilt.com/sale/women-apparel/theory-1093?origin=sale|nav"
+
+  [deal_1, deal_2]
+  end
+end
