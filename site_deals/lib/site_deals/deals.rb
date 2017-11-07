dd0839b707b2cba5b94f98726904d9ac6543b73d
Lara Sanders <larasanders@gmail.com>
2017-11-07 21:16:54 +0000

    creating self_scrape methods for Gilt and Shopbop

--- site_deals/lib/site_deals/deals.rb
+++ site_deals/lib/site_deals/deals.rb
@@ -1,33 +1,39 @@
+require 'pry'
 class SiteDeals::Deals
-  attr_accessor :name, :price, :availability, :url
+  attr_accessor :name, :price, :availability, :url, :title
 
   def self.today
     self.scrape_deals
-  #   puts "Today's Site Deals"
-  #   puts <<-DOC.gsub /^\s*/, ''
-  #   1. Joie and soft Joie -ruelala
-  #   2. Theory - Gilt
-  # DOC
 end
 
 def self.scrape_deals
   deals = []
 
-  deals << self.scrape.ruelala
-  # deal_1 = self.new
-  # deal_1.name = "Joie and soft Joie"
-  # deal_1.availability = true
-  # deal_1.url = "https://www.ruelala.com/boutique/collection/105984/192960/?dsi=BTQ-f142a7af-3767-4f15-b365-f6757b52ba5d"
-  #
-  # deal_2 = self.new
-  # deal_2.name = "Theory"
-  # deal_2.availability = true
-  # deal_2.url = "https://www.gilt.com/sale/women-apparel/theory-1093?origin=sale|nav"
-
+  deals << self.scrape_shopbop
+  deals << self.scrape_gilt
   deals
   end
 
-  def self.scrape_ruelala
-    doc = Nokogiri::HTML(open("https://ruelala.com"))
-    binding.pry
+  def self.scrape_shopbop
+
+  deals = self.new
+  doc = Nokogiri::HTML(open("https://www.shopbop.com/new-today/br/v=1/50579.htm"))
+  deals.name = doc.search("div.brand").first.text
+  deals.title = doc.search("div.title").first.text
+  deals.price = doc.search("span.retail-price").first.text
+  deals.url = doc.search("span.productBrowseMainImage img").attribute("src").value
+  deals
+end
+
+def self.scrape_gilt
+  # deals = self.new
+  doc = Nokogiri::HTML(open("https://www.gilt.com/sale/women-apparel/hero-lafayette-148-new-york-3666?origin=sale|homepage|hero"))
+   binding.pry
+   deals.name = doc.search("span.brand-name")
+   deals.title = doc.search("h1.name fn product-name").first.text
+  # deals.price = doc.search("span.retail-price").first.text
+  # deals.url = doc.search("span.productBrowseMainImage img").attribute("src").value
+  # deals
+
+end
 end
