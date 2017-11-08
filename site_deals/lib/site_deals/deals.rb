4b2425e314acba8cdbd8701c63b5d966427154cf
Lara Sanders <larasanders@gmail.com>
2017-11-08 15:43:15 +0000

    Finished scrape methods, cleaned up output with strip

--- site_deals/lib/site_deals/deals.rb
+++ site_deals/lib/site_deals/deals.rb
@@ -18,22 +18,24 @@ def self.scrape_deals
 
   deals = self.new
   doc = Nokogiri::HTML(open("https://www.shopbop.com/new-today/br/v=1/50579.htm"))
-  deals.name = doc.search("div.brand").first.text
-  deals.title = doc.search("div.title").first.text
-  deals.price = doc.search("span.retail-price").first.text
-  deals.url = doc.search("span.productBrowseMainImage img").attribute("src").value
+  deals.name = doc.search("div.brand").first.text.strip
+  deals.title = doc.search("div.title").first.text.strip
+  deals.price = doc.search("span.retail-price").first.text.strip
+  deals.url = doc.search("span.productBrowseMainImage img").attribute("src").value.strip
   deals
 end
 
 def self.scrape_gilt
-  # deals = self.new
+   deals = self.new
   doc = Nokogiri::HTML(open("https://www.gilt.com/sale/women-apparel/hero-lafayette-148-new-york-3666?origin=sale|homepage|hero"))
-   binding.pry
-   deals.name = doc.search("span.brand-name")
-   deals.title = doc.search("h1.name fn product-name").first.text
-  # deals.price = doc.search("span.retail-price").first.text
-  # deals.url = doc.search("span.productBrowseMainImage img").attribute("src").value
-  # deals
+
+   deals.name = doc.search("a span.brand-name-text").first.text.strip
+   deals.title = doc.search("h1 a[href]").first.text.strip
+   deals.price = doc.search("span.price").first.text.strip
+   deals.url = doc.search("h1 a[href]").first.attr("href").strip
+    #  binding.pry
+  deals
+
 
 end
 end
