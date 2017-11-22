8ed04181322868d937d132a169e4bf74b32859fe
Lara Sanders <larasanders@gmail.com>
2017-11-22 16:07:04 +0000

    refactored code, simplified code - focused on one site only

--- site_deals/lib/site_deals/cli.rb
+++ site_deals/lib/site_deals/cli.rb
@@ -8,60 +8,32 @@ class SiteDeals::CLI
   end
 
   def list_deals
-    @deals= SiteDeals::Deals.today
-    # @deals.each.with_index(1) do |deal, i|
-    #   puts "#{i}. #{deal}.name - #{deal}.title- #{deal}.price"
-    #   puts "#{the_deal}"
-      @deals.each.with_index(1) do |deal, i|
-      # dealer.each.with_index(1) do |deal, i|
-    #  binding.pry
-         puts  "#{i}. #{deal.name}"
-        # puts @deals[:name]
-        # @deals.each.with_index(1) do |d, i|
-        #  binding.pry
-          # d.each do |k, v|
-          #   puts ="#{i}. #{k} => #{v}"
-        #binding.pry
+    @@deals= SiteDeals::Deals.today
+    @@deals.each.with_index(1) do |deal, i|
+      if deal !=0
+        puts  "#{i}. #{deal.name}"
       end
+    end
 
-end
+    def menu
+      @deals = SiteDeals::Deals.today
+      input = nil
+      while input != "exit"
+        puts "Please select brand to see products or type list to see brands again or type exit:"
+        input = gets.strip.downcase
+        if input.to_i.between?(1,@@deals.count)
+          the_deal = @@deals[input.to_i-1]
+          puts "#{the_deal.name} - #{the_deal.title} - #{the_deal.price}"
+          puts "For this product, please visit: #{the_deal.url}"
+        elsif input == "list"
+          list_deals
+        else
+          puts "I didn't understand. Please select brand, list, or exit."
+        end
+      end
+    end
 
-
-
-
-  def menu
-    @deals = SiteDeals::Deals.today
-    input = nil
-  #  list_deals.each.with_index(1) do |deal, i|
-    while input != "exit"
-      puts "Please select brand to see products or type list to see brands again or type exit:"
-      input = gets.strip.downcase
-#binding.pry
-      if input.to_i.between?(1,@deals.count)
-        the_deal = @deals[input.to_i-1]
-
-            puts "#{the_deal.name} - #{the_deal.title} - #{the_deal.price}"
-            puts "For this product, please visit https://www.shopbop.com#{the_deal.url}"
-
-      elsif input.to_i == 2
-          the_deal = @deals[input.to_i-1]
-
-              puts "#{the_deal.name} - #{the_deal.title} - #{the_deal.price}"
-              puts "For this product, please visit https://www.revolve.com#{the_deal.url}"
-      elsif input == "list"
-        list_deals
-      else
-        puts "I didn't understand. Please select brand, list, or exit."
-        # binding.pry
-
-
-end
-end
-end
-
-
-
-  def goodbye
-    puts "See you tomorrow for more deals!!!"
+    def goodbye
+      puts "See you tomorrow for more deals!!!"
+    end
   end
-end
--- site_deals/lib/site_deals/deals.rb
+++ site_deals/lib/site_deals/deals.rb
@@ -1,49 +1,31 @@
 require 'pry'
 class SiteDeals::Deals
-  attr_accessor :name, :price, :url, :title,
+  attr_accessor :name, :price, :availability, :url, :title
 
-def initialize(name, price, url, title)
-  @name = name
-  @price = price
-  @url = item_url
-  @title = title
-end
+  @@deals = []
+
+  def initialize(name = nil, title = nil, price = nil, url = nil)
+    @name = name
+    @price = price
+    @url = url
+    @title = title
+    #binding.pry
+    @@deals << self
+  end
 
   def self.today
     self.scrape_deals
-end
-
-def self.scrape_deals
-   deals = []
-
-  deals << self.scrape_shopbop
-  deals << self.scrape_revolve
-  # deals << self.get_brands
-  # deals.split(",")
-
   end
 
-  def self.scrape_shopbop
-  doc = Nokogiri::HTML(open("https://www.shopbop.com/new-today/br/v=1/50579.htm"))
-  name = doc.search("div.brand").first.text.strip
-  title = doc.search("div.title").first.text.strip
-  price = doc.search("span.retail-price").first.text.strip
-  url = doc.search("div.info.clearfix a[href]").attr("href").value
-  #binding.pry
-  self.new(name, price, title, url)
-end
+  def self.scrape_deals
+    @@deals << self.scrape_revolve
+  end
 
-def self.scrape_revolve
+  def self.scrape_revolve
+    doc = Nokogiri::HTML(open("http://www.revolve.com/new/all-new-items/br/7f5946/?navsrc=subnew_i1"))
+    info = doc.css("div.container .js-plp-container").each do |info|
+      self.new(info.css("div.js-plp-brand").text, info.css("div.js-plp-name").text, info.css("a span.plp_price").text, info.css("a img.js-plp-image").attr("src").value)
 
-  doc = Nokogiri::HTML(open("http://www.revolve.com/new/all-new-items/br/7f5946/?navsrc=subnew_i1"))
-
-  name = doc.search("a div.plp-brand.js-plp-brand").first.children.text.strip
-   title = doc.search("a div.plp-name.h1.plp-name").first.children.text.strip
-
-   price = doc.search("a span.plp_price").first.children.text
-#binding.pry
-   url =  doc.search("div.u-text-decoration-none a[href]").attr("href").value
-   self.new(name, price, title, url)
-
-end
+    end
+  end
 end
